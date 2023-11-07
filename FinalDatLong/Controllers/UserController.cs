c769dddfce6ce0faf0fbaad1a708538ad81c3b34
datnguyen13021302 <21280103e0017@student.tdmu.edu.vn>
2023-11-07 11:28:54 +0700

    #

--- FinalDatLong/Controllers/UserController.cs
+++ FinalDatLong/Controllers/UserController.cs
@@ -10,13 +10,38 @@ namespace FinalDatLong.Controllers
     public class UserController : Controller
     {
         // GET: User
+        Model1 db = new Model1();
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public ActionResult DangKy()
         {
+
             return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy(Partient Model)
+        {
+            if (ModelState.IsValid)
+            {
+                var kh = db.Partients.FirstOrDefault(k => k.UserName == Model.UserName);
+                if (kh != null)
+                {
+                    ModelState.AddModelError("UserName", "Ten tai Khoan ton tai");
+                    return View(Model);
+                }
+
+                db.Partients.Add(Model);
+                db.SaveChanges();
+                return View("DangNhap");
+
+            }
+
+            return View(Model);
+
+
         }
         [HttpGet]
         public ActionResult DangNhap()
@@ -26,25 +51,25 @@ namespace FinalDatLong.Controllers
         [HttpPost]
         public ActionResult DangNhap(UserLogin user)
         {
-            //if (ModelState.IsValid)
-            //{
-            //    var u = db.KHACHHANG.FirstOrDefault(k => k.Taikhoan == user.Username && k.Matkhau == user.Password);
-            //    if (u != null)
-            //    {
-            //        Session["TaiKhoan"] = u;
-            //        ViewBag.ThongBao = "Đăng Nhập Thành Công";
-            //        return Redirect("~/");
-            //    }
-            //    else
-            //    {
-            //        ModelState.AddModelError("Password", "Error");
-            //    }
-            //}
+            if (ModelState.IsValid)
+            {
+                var u = db.Partients.FirstOrDefault(k => k.UserName == user.Username && k.Password == user.Password);
+                if (u != null)
+                {
+                    Session["UserName"] = u;
+                    ViewBag.ThongBao = "Đăng Nhập Thành Công";
+                    return Redirect("~/");
+                }
+                else
+                {
+                    ModelState.AddModelError("Password", "Error");
+                }
+            }
             return View();
         }
         public ActionResult DangXuat()
         {
-            Session["TaiKhoan"] = null;
+            Session["UserName"] = null;
             return RedirectToAction("Index", "Booking");
         }
     }
--- FinalDatLong/Models/Partient.cs
+++ FinalDatLong/Models/Partient.cs
@@ -1,4 +1,4 @@
-namespace FinalDatLong.Models
+﻿namespace FinalDatLong.Models
 {
     using System;
     using System.Collections.Generic;
@@ -16,14 +16,19 @@ namespace FinalDatLong.Models
             Bookings = new HashSet<Booking>();
             Treatments = new HashSet<Treatment>();
         }
-
-        [Required]
+        [Required(ErrorMessage = "Tài khoản không được đỡ trống")]
         [StringLength(50)]
         public string UserName { get; set; }
 
-        [Required]
-        [StringLength(50)]
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        [StringLength(255, ErrorMessage = " Mật Khẩu từ 8 đến 255 kí tự ", MinimumLength = 8)]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+    ErrorMessage = "Mật khẩu tối thiểu tám ký tự, ít nhất một chữ hoa, một chữ thường, một số và một ký tự đặc biệt")]
         public string Password { get; set; }
+
+        [NotMapped]
+        [Compare("Password", ErrorMessage = " Mật Khẩu nhập lại không khớp")]
+        public string MatKhauNL { get; set; }
 
         [Key]
         [StringLength(20)]
@@ -32,7 +37,8 @@ namespace FinalDatLong.Models
         [StringLength(255)]
         public string FullName { get; set; }
 
-        [StringLength(255)]
+        [Required(ErrorMessage = "Email không được để trống")]
+        [StringLength(100)]
         public string Email { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
