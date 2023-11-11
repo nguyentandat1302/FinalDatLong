c638b0b504a6c121c51b449ed58bf9886566fedb
datnguyen13021302 <21280103e0017@student.tdmu.edu.vn>
2023-11-12 02:11:03 +0700

    #

---
+++ FinalDatLong/Controllers/ALLController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalDatLong.Controllers
+{
+    public class ALLController : Controller
+    {
+        // GET: ALL
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contacts() 
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
